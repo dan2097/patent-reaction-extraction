057acb011dde5301df07d35b58dceb786864ab66
Daniel Lowe <dl387@cam.ac.uk>
2011-03-08 20:02:01 +0000

    First attempt at recogniseing reactants from POS Crude attempt at recognising aliases in heading text

---
+++ src/main/java/dan2097/org/bitbucket/reactionextraction/ExperimentalSectionParser.java
@@ -0,0 +1,208 @@
+package dan2097.org.bitbucket.reactionextraction;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+
+import uk.ac.cam.ch.wwmm.opsin.XOMTools;
+import dan2097.org.bitbucket.utility.ChemicalTaggerAtrs;
+import dan2097.org.bitbucket.utility.ChemicalTaggerTags;
+import dan2097.org.bitbucket.utility.Utils;
+import dan2097.org.bitbucket.utility.XMLAtrs;
+import dan2097.org.bitbucket.utility.XMLTags;
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Elements;
+import nu.xom.Nodes;
+
+public class ExperimentalSectionParser {
+
+	private final Map<Element, Chemical> moleculeToChemicalMap = new HashMap<Element, Chemical>();
+	private final Map<String, Chemical> aliasToChemicalMap;
+	private final static Pattern matchIdentifier = Pattern.compile("((\\d+[a-z]?)|[\\(\\{\\[](\\d+[a-z]?|.*\\d+)[\\)\\}\\]])\\s*$");
+	private final Chemical titleCompound;
+	
+	public ExperimentalSectionParser(Element headingEl, Map<String, Chemical> aliasToChemicalMap) {
+		String headingText = headingEl.getAttributeValue(XMLAtrs.TITLE);
+		titleCompound = extractChemicalFromHeading(headingText);
+		this.aliasToChemicalMap = aliasToChemicalMap;
+		if (titleCompound!=null){
+			attemptToExtractAliasFromTitleText(headingText);
+			List<Paragraph> paragraphs = new ArrayList<Paragraph>();
+			for (Element p :  XOMTools.getChildElementsWithTagName(headingEl, XMLTags.P)) {
+				Paragraph para = new Paragraph(p);
+				if (!para.getTaggedString().equals("")){
+					paragraphs.add(para);
+					generateMoleculeToChemicalMap(para);
+				}
+			}
+			determineReactions(paragraphs);
+		}
+	}
+
+	private Chemical extractChemicalFromHeading(String title) {
+		if (title==null){
+			throw new IllegalArgumentException("Input title text was null");
+		}
+		List<String> name = Utils.getSystematicChemicalNamesFromText(title);
+		if (name.size()==1){
+			return new Chemical(name.get(0));
+		}
+		return null;
+	}
+
+	private void attemptToExtractAliasFromTitleText(String headingText) {
+		Matcher m = matchIdentifier.matcher(headingText);
+		if (m.find()){
+			if (m.group(1)!=null){
+				aliasToChemicalMap.put(m.group(1), titleCompound);
+			}
+			else if (m.group(2)!=null){
+				aliasToChemicalMap.put(m.group(2), titleCompound);
+			}
+			else{
+				throw new RuntimeException("identifier regex is malformed");
+			}
+		}
+	}
+
+	private void generateMoleculeToChemicalMap(Paragraph para) {
+		List<Element> moleculeEls = findAllMolecules(para);
+		for (Element moleculeEl : moleculeEls) {
+			Chemical chem = generateChemicalsFromMoleculeElsAndLocalInformation(moleculeEl);
+			moleculeToChemicalMap.put(moleculeEl, chem);
+		}
+	}
+
+	private Chemical generateChemicalsFromMoleculeElsAndLocalInformation(Element moleculeEl) {
+		Chemical chem = new Chemical(findMoleculeName(moleculeEl));
+		ChemicalPropertyDetermination.determineProperties(chem, moleculeEl);
+		return chem;
+	}
+	
+	/**
+	 * Finds the chemical name of the chemical described by a chemical tagger MOLECULE tag
+	 * @param molecule
+	 * @return
+	 */
+	private String findMoleculeName(Element molecule) {
+		if (!molecule.getLocalName().equals(ChemicalTaggerTags.MOLECULE_Container)) {
+			throw new IllegalArgumentException("argument was not a molecule");
+		}
+		
+		Element singleWordName = molecule.getFirstChildElement(ChemicalTaggerTags.OSCAR_CM);
+		if (singleWordName != null) {
+			return singleWordName.getValue();
+		}
+		
+		Element multiWordName = molecule.getFirstChildElement(ChemicalTaggerTags.OSCARCM_Container);
+		if (multiWordName != null) {
+			Elements multiWordNameWords = multiWordName.getChildElements(ChemicalTaggerTags.OSCAR_CM);
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < multiWordNameWords.size(); i++) {
+				builder.append(multiWordNameWords.get(i).getValue());
+				if (i < multiWordNameWords.size()-1) {
+					builder.append(" ");
+				}
+			}
+			return builder.toString();	
+		}
+		throw new RuntimeException("malformed molecule");
+	}
+
+
+	/**
+	 * Creates a list of all MOLECULE elements
+	 * @return
+	 */
+	private List<Element> findAllMolecules(Paragraph paragraph) {
+		List <Element> mols = new ArrayList<Element>();
+		Document chemicalTaggerResult =paragraph.getTaggedSentencesDocument();
+		Nodes molecules = chemicalTaggerResult.query("//" + ChemicalTaggerTags.MOLECULE_Container);
+		for (int i = 0; i < molecules.size(); i++) {
+			Element molecule = (Element) molecules.get(i);
+			mols.add(molecule);
+		}
+		return mols;
+	}
+
+
+	private List<Reaction> determineReactions(List<Paragraph> paragraphs) {
+		List<Reaction> reactions = new ArrayList<Reaction>();
+		String titleCompoundInChI =titleCompound.getInchi();
+		for (Paragraph paragraph : paragraphs) {
+			Element taggedDocRoot = paragraph.getTaggedSentencesDocument().getRootElement();
+			List<Element> sentences = XOMTools.getChildElementsWithTagName(taggedDocRoot, ChemicalTaggerTags.SENTENCE_Container);
+			for (Element sentence : sentences) {
+				Set<Element> products = new HashSet<Element>();
+				for (String xpath : Xpaths.yieldXPaths) {
+					Nodes synthesizedMolecules = sentence.query(xpath);
+					for (int i = 0; i < synthesizedMolecules.size(); i++) {
+						products.add((Element) synthesizedMolecules.get(i));
+					}
+				}
+				if (products.size() >0){
+					Set<Element> reactants = new HashSet<Element>();
+					for (Element product : products) {
+						for (String xpath : Xpaths.reactantXpaths) {
+							Nodes reactantMolecules = product.query(xpath);
+							for (int i = 0; i < reactantMolecules.size(); i++) {
+								reactants.add((Element) reactantMolecules.get(i));
+							}
+						}
+					}
+			
+					Reaction reaction = new Reaction();
+					for (Element product : products) {
+						Chemical chem = moleculeToChemicalMap.get(product);
+						if (chem!=null){
+							reaction.addProduct(chem);
+						}
+						else{
+							//TODO resolve symbolic chemical names (could however be an unresolvable chemical name)
+						}
+					}
+					for (Element reactant : reactants) {
+						Chemical chem = moleculeToChemicalMap.get(reactant);
+						if (chem!=null){
+							if (!ChemicalTaggerAtrs.SOLVENT_ROLE_VAL.equals(reactant.getAttributeValue(ChemicalTaggerAtrs.ROLE_ATR))){
+								reaction.addReactant(chem);
+							}
+							else{
+								reaction.addSpectator(chem);
+							}
+						}
+						else{
+							//TODO resolve symbolic chemical names (could however be an unresolvable chemical name)
+						}
+					}
+					if (reaction.getProducts().size()>0 || reaction.getReactants().size()>0){
+						try {
+							File f = File.createTempFile("reaction", ".png", new File("C:/My Documents/workspace/PatentReactionExtractor/temp"));
+							ReactionDepicter.depictReaction(reaction, f);
+							File xml = new File("temp/"+f.getName().substring(0, f.getName().length()-4) +".xml");
+							FileOutputStream out = new FileOutputStream(xml);
+							IOUtils.write(sentence.toXML(), out);
+							IOUtils.closeQuietly(out);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+
+					reactions.add(reaction);
+				}
+			}
+		}
+		return reactions;
+	}
+}
--- src/main/java/dan2097/org/bitbucket/reactionextraction/Xpaths.java
+++ src/main/java/dan2097/org/bitbucket/reactionextraction/Xpaths.java
@@ -6,12 +6,21 @@ import java.util.List;
 public class Xpaths {
 
 	public static List<String> yieldXPaths;
+	public static List<String> reactantXpaths;
 	static{
 		yieldXPaths = new ArrayList<String>();
+		reactantXpaths = new ArrayList<String>();
 		/*A nounphrase containing the returrned molecule at the start of a synthesize phrase followed by something thing like "is/was synthesised"*/
-		yieldXPaths.add("//ActionPhrase[@type='Synthesize']/NounPhrase[fn:position() = 1][following-sibling::*[1][local-name()='VerbPhrase'][VBD|VBZ][VB-SYNTHESIZE]]/*[self::MOLECULE or self::UNNAMEDMOLECULE]");
+		yieldXPaths.add(".//ActionPhrase[@type='Synthesize']/NounPhrase[fn:position() = 1][following-sibling::*[1][local-name()='VerbPhrase'][VBD|VBZ][VB-SYNTHESIZE]]/*[self::MOLECULE or self::UNNAMEDMOLECULE or self::MIXTURE ]");
 		
 		/*A nounphrase containing the returned molecule in a yield phrase*/
-		yieldXPaths.add("//ActionPhrase[@type='Yield']/NounPhrase/*[self::MOLECULE or self::UNNAMEDMOLECULE]");
+		yieldXPaths.add(".//ActionPhrase[@type='Yield']/NounPhrase/*[self::MOLECULE or self::UNNAMEDMOLECULE or self::MIXTURE ]");
+		
+		
+		
+		
+		
+		/*synthesized by/from chemical*/
+		reactantXpaths.add("../following-sibling::*[1]/VB-SYNTHESIZE/following-sibling::PrepPhrase/*[self::IN-BY or self::IN-FROM]/following-sibling::*[1]/*[self::MOLECULE or self::UNNAMEDMOLECULE or self::MIXTURE ]");
 	}
 }
