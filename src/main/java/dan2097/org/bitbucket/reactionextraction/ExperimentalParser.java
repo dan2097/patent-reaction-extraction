1338ae16a1b5783b2c4993f6b2138407984e86fc
Daniel Lowe <dl387@cam.ac.uk>
2011-04-14 16:59:19 +0100

    Updated to latest version of OSCAR and chemical tagger Updated reaction serialisation code

--- src/main/java/dan2097/org/bitbucket/reactionextraction/ExperimentalParser.java
+++ src/main/java/dan2097/org/bitbucket/reactionextraction/ExperimentalParser.java
@@ -1,15 +1,42 @@
 package dan2097.org.bitbucket.reactionextraction;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import nu.xom.Element;
 
+import org.apache.commons.io.FileUtils;
+
 public class ExperimentalParser {
 	private final Map<String, Chemical> aliasToChemicalMap = new HashMap<String, Chemical>();
+	private final List<Reaction> documentReactions = new ArrayList<Reaction>();
 
 	public void parseExperimentalSection(Element headingElementToProcess) {
-		new ExperimentalSectionParser(headingElementToProcess, aliasToChemicalMap);
+		ExperimentalSectionParser sectionparser = new ExperimentalSectionParser(headingElementToProcess, aliasToChemicalMap);
+		documentReactions.addAll(sectionparser.getReactions());
+	}
+
+	public void serialize(File directory) throws IOException {
+		if (!directory.exists()){
+			FileUtils.forceMkdir(directory);
+		}
+		if (!directory.isDirectory()){
+			throw new IllegalArgumentException("A directory was expected");
+		}
+		for (Reaction reaction : documentReactions) {
+			if (reaction.getProducts().size()>0 || reaction.getReactants().size()>0){
+				try {
+					File f = File.createTempFile("reaction", ".png", directory);
+					ReactionDepicter.depictReaction(reaction, f);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 		
 	}
 
