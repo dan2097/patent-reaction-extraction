05dbc7a9e99f51fefb6cd4101779ee29a163936b
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-04-20 16:28:29 +0100

    Synchronized tag list with chemical tagger's vocabulary

--- src/main/java/dan2097/org/bitbucket/utility/ChemicalTaggerTags.java
+++ src/main/java/dan2097/org/bitbucket/utility/ChemicalTaggerTags.java
@@ -2,22 +2,22 @@ package dan2097.org.bitbucket.utility;
 
 public class ChemicalTaggerTags {
 	
-	/** OSCAR cardinal number e.g. compound 5*/
-	public static final String OSCAR_CD = "OSCAR-CD";
+	/** A number followed by a letter*/
+	public static final String CD_ALPHANUM = "CD-ALPHANUM";
 	
 	/** OSCAR chemical adjective e.g. alcoholic*/
 	public static final String OSCAR_CJ = "OSCAR-CJ";
 	
 	/** OSCAR reaction e.g. methylated*/
 	public static final String OSCAR_RN = "OSCAR-RN";
-
-	/** OSCAR chemical prefix e.g. '1,3-'*/
-	public static final String OSCAR_CPR = "OSCAR-CPR";
+	
+	/** OSCAR enzyme*/
+	public static final String OSCAR_ASE = "OSCAR-ASE";
 	
 	/** OSCAR ontology term e.g. anything present in ChEBI*/
 	public static final String OSCAR_ONT = "OSCAR-ONT";
 
-	/** Unknown not currently used*/
+	/** Unicode trademark*/
 	public static final String TM_UNICODE = "TM-UNICODE";
 	
 	/** A unicode cardinal number?*/
@@ -25,6 +25,9 @@ public class ChemicalTaggerTags {
 	
 	/** A chemical adjective*/
 	public static final String JJ_CHEM = "JJ-CHEM";
+	
+	/** A chemical adjective that defines an unnamed molecule e.g. "title" */
+	public static final String JJ_COMPOUND = "JJ-COMPOUND";
 
 	
 	// Prepositions
@@ -46,16 +49,17 @@ public class ChemicalTaggerTags {
 	public static final String IN_OFF = "IN-OFF";
 
 	//Modified Nouns
-	public static final String NN_EXAMPLE = "NN-EXAMPLE";
 	public static final String NN_STATE = "NN-STATE";
 	public static final String NN_TIME = "NN-TIME";
-
+	
 	/** contains the text typically describing the units used for mass */
 	public static final String NN_MASS = "NN-MASS";
-	public static final String NN_MOLAR = "NN-MOLAR";
 	
 	/** contains the text typically describing the units used for amount */
 	public static final String NN_AMOUNT = "NN-AMOUNT";
+
+	public static final String NN_MOLAR = "NN-MOLAR";
+	
 	public static final String NN_ATMOSPHERE = "NN-ATMOSPHERE";
 	public static final String NN_EQ = "NN-EQ";
 	
@@ -72,6 +76,7 @@ public class ChemicalTaggerTags {
 	public static final String NN_VACUUM = "NN-VACUUM";
 	public static final String NN_CYCLE = "NN-CYCLE";
 	public static final String NN_TIMES = "NN-TIMES";
+	public static final String NN_EXAMPLE = "NN-EXAMPLE";
 
 	//Not really Oscar-cm.. but need to be fixed
 	/** contains the text for one word of chemical name */
@@ -170,6 +175,8 @@ public class ChemicalTaggerTags {
 	//Yield Tokens
 	public static final String NN_YIELD = "NN-YIELD";
 	//Misc Tokens mainly to replace characters that are not markup friendly
+	// Conjunctive Adverbs
+	public static final String RB_CONJ = "RB-CONJ";
 	public static final String COLON = "COLON";
 	public static final String COMMA = "COMMA";
 	public static final String APOST = "APOST";
@@ -179,104 +186,29 @@ public class ChemicalTaggerTags {
 	public static final String NN_PERCENT = "NN-PERCENT";
 	public static final String LSQB = "LSQB";
 	public static final String RSQB = "RSQB";
+	
+	//The determiner 'the';
+	public static final String DT_THE = "DT-THE";
 
 	public static final String LRB = "-LRB-";
 	public static final String RRB = "-RRB-";
 
-	//Brown Corpus Tokens
-
-	// Pre-qualifier (quite, rather)
-	public static final String ABL = "ABL";
-
-	// Pre-quantifier (half, all)
-	public static final String ABN = "ABN";
-
-	// Pre-quantifier (both)
-	public static final String ABX = "ABX";
-
-	// Post-determiner (many, several, next)
-	public static final String AP = "AP";
-
-	// Article (a, the, no)
-	public static final String AT = "AT";
-
-	// Be
-	public static final String BE = "BE";
-
-	// Were
-	public static final String BED = "BED";
-
-	// Was
-	public static final String BEDZ = "BEDZ";
-
-	// Being
-	public static final String BEG = "BEG";
-
-	// Am
-	public static final String BEM = "BEM";
-
-	// Been
-	public static final String BEN = "BEN";
-
-	// Are, art
-	public static final String BER = "BER";
-
-	// Is
-	public static final String BEZ = "BEZ";
+	//Penn Treebank Tokens
 
 	// Coordinating conjunction (and, or)
 	public static final String CC = "CC";
 
-	/** Cardinal numeral (one, two, 2, etc.) */
+	// Cardinal numeral (one, two, 2, etc.)
 	public static final String CD = "CD";
-
-	// Subordinating conjunction (if, although)
-	public static final String CS = "CS";
-
-	// Do
-	public static final String DO = "DO";
-
-	// Did
-	public static final String DOD = "DOD";
-
-	// Does
-	public static final String DOZ = "DOZ";
 
 	// Singular determiner/quantifier (this, that)
 	public static final String DT = "DT";
-
-
-	// Singular determiner/quantifier (this, that)
-	public static final String DT_THE = "DT-THE";
-	// Singular or plural determiner/quantifier (some, any)
-	public static final String DTI = "DTI";
-
-	// Plural determiner (these, those)
-	public static final String DTS = "DTS";
-
-	// Determiner/double conjunction (either)
-	public static final String DTX = "DTX";
 
 	// Existential there
 	public static final String EX = "EX";
 
 	// Foreign word (hyphenated before regular tag)
 	public static final String FW = "FW";
-
-	// Foreign word (hyphenated before regular tag)
-	public static final String FW_IN = "FW-IN";
-
-	// Have
-	public static final String HV = "HV";
-
-	// Had (past tense)
-	public static final String HVD = "HVD";
-
-	// Having
-	public static final String HVG = "HVG";
-
-	// Had (past participle)
-	public static final String HVN = "HVN";
 
 	// Preposition
 	public static final String IN = "IN";
@@ -290,84 +222,35 @@ public class ChemicalTaggerTags {
 	// Semantically superlative adjective (chief, top)
 	public static final String JJS = "JJS";
 
-	// Morphologically superlative adjective (biggest)
-	public static final String JJT = "JJT";
+	// List item marker 
+	public static final String LS = "LS";
 
 	// Modal auxiliary (can, should, will)
 	public static final String MD = "MD";
 
-	// Cited word (hyphenated after regular tag)
-	public static final String NC = "NC";
-
 	// Singular or mass noun
 	public static final String NN = "NN";
-
-	// Possessive singular noun
-	public static final String NN$ = "NN$";
 
 	// Plural noun
 	public static final String NNS = "NNS";
 
-	// Possessive plural noun
-	public static final String NNS$ = "NNS$";
-
-	// Proper noun or part of name phrase
-	public static final String NP = "NP";
-
-
 	// Proper noun or part of name phrase
 	public static final String NNP = "NNP";
 
+	// Proper noun, plural 
+	public static final String NNPS = "NNPS";
 
-	// Possessive proper noun
-	public static final String NP$ = "NP$";
+	//Predeterminer
+	public static final String PDT = "PDT";
 
-	// Plural proper noun
-	public static final String NPS = "NPS";
+	// Possessive ending 
+	public static final String POS = "POS";
 
-	// Possessive plural proper noun
-	public static final String NPS$ = "NPS$";
-
-	// Adverbial noun (home, today, west)
-	public static final String NR = "NR";
-
-	// Ordinal numeral (first, 2nd)
-	public static final String OD = "OD";
-
-	// Nominal pronoun (everybody, nothing)
-	public static final String PN = "PN";
-
-	// Possessive nominal pronoun
-	public static final String PN$ = "PN$";
-
-	// Possessive personal pronoun (my, our)
-	public static final String PP$ = "PP$";
-
-	// Second (nominal) possessive pronoun (mine, ours)
-	public static final String PP$$ = "PP$$";
-
-	// Singular reflexive/intensive personal pronoun (myself)
-	public static final String PPL = "PPL";
-
-	// Plural reflexive/intensive personal pronoun (ourselves)
-	public static final String PPLS = "PPLS";
-
+	//Personal pronoun
 	public static final String PRP = "PRP";
 
-	// Objective personal pronoun (me, him, it, them)
-	public static final String PPO = "PPO";
-
-	// 3rd. singular nominative pronoun (he, she, it, one)
-	public static final String PPS = "PPS";
-
-	// Other nominative personal pronoun (i, we, they, you)
-	public static final String PPSS = "PPSS";
-
-	// Qualifier (very, fairly)
-	public static final String QL = "QL";
-
-	// Post-qualifier (enough, indeed)
-	public static final String QLP = "QLP";
+	//Possessive pronoun
+	public static final String PRP$ = "PRP$";
 
 	// Adverb
 	public static final String RB = "RB";
@@ -375,23 +258,13 @@ public class ChemicalTaggerTags {
 	// Comparative adverb
 	public static final String RBR = "RBR";
 
-	// Conjunctive Adverbs
-	public static final String RB_CONJ = "RB-CONJ";
-
-	// Superlative adverb
-	public static final String RBT = "RBT";
-
 	// Superlative adverb
 	public static final String RBS = "RBS";
-
-
-	// Nominal adverb (here, then, indoors)
-	public static final String RN = "RN";
 
 	// Adverb/particle (about, off, up)
 	public static final String RP = "RP";
 
-
+	// Symbol
 	public static final String SYM = "SYM";
 
 	// Infinitive marker to
@@ -403,9 +276,6 @@ public class ChemicalTaggerTags {
 	// Verb, base form
 	public static final String VB = "VB";
 
-	public static final String VBP = "VBP";
-		;
-
 	// Verb, past tense
 	public static final String VBD = "VBD";
 
@@ -415,31 +285,23 @@ public class ChemicalTaggerTags {
 	// Verb, past participle
 	public static final String VBN = "VBN";
 
+	// Verb, non-3rd person singular present
+	public static final String VBP = "VBP";
+
 	// Verb, 3rd. singular present
 	public static final String VBZ = "VBZ";
 
-	// Wh- determiner (what, which)
+	// Wh- determiner (which, that)
 	public static final String WDT = "WDT";
+
+	// wh- pronoun (what, who, whom)
+	public static final String WP = "WP";
 
 	// Possessive wh- pronoun (whose)
 	public static final String WP$ = "WP$";
 
-
-	// Objective wh- pronoun (whom, which, that)
-	public static final String WPO = "WPO";
-
-	// Nominative wh- pronoun (who, which, that)
-	public static final String WPS = "WPS";
-
-
-	// Wh- qualifier (how)
-	public static final String WQL = "WQL";
-
 	// Wh- adverb (how, where, when)
 	public static final String WRB = "WRB";
-
-	public static final String PDT = "PDT";
-	
 
 	//Container nodes
 	/**Container for a parsed sentence*/
@@ -480,6 +342,8 @@ public class ChemicalTaggerTags {
 	/** contains one or more cardinal numbers and NN-MASS */
 	public static final String MASS_Container = "MASS";
 	
+	public static final String PERCENT_Container = "PERCENT";
+	
 	/** contains one or more cardinal numbers and NN-VOL */
 	public static final String VOLUME_Container = "VOLUME";
 	
@@ -502,6 +366,8 @@ public class ChemicalTaggerTags {
 	
 	/**Holds among other things amount nodes*/
 	public static final String QUANTITY_Container = "QUANTITY";
+
+	public static final String OSCARONT_Container = "OSCARONT";
 	
 	/**Holds phrases/punctuation that together form the action phrase*/
 	public static final String ACTIONPHRASE_Container = "ActionPhrase";
