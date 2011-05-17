116c10ac11ee4dacba92379258b1e85edbfe7fa8
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-05-17 13:10:11 +0100

    Factored stoichiometry detection code into a new class and begans using parent pom

--- src/main/java/dan2097/org/bitbucket/reactionextraction/ExperimentalSectionParser.java
+++ src/main/java/dan2097/org/bitbucket/reactionextraction/ExperimentalSectionParser.java
@@ -2,16 +2,11 @@ package dan2097.org.bitbucket.reactionextraction;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Map.Entry;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
@@ -38,7 +33,6 @@ public class ExperimentalSectionParser {
 	private final Chemical titleCompound;
 	private final List<Element> paragraphEls;
 	private final List<Reaction> reactions = new ArrayList<Reaction>();
-	private final Pattern matchAmount = Pattern.compile("([mn\u00b5]|pico|nano|micro|milli)?mol[e]?[s]?");
 
 	public ExperimentalSectionParser(Chemical titleCompound, List<Element> paragraphEls, Map<String, Chemical> aliasToChemicalMap) {
 		this.titleCompound = titleCompound;
@@ -58,7 +52,6 @@ public class ExperimentalSectionParser {
 				}
 			}
 			reactions.addAll(determineReactions(paragraphs));
-			processReactionStoichiometry();
 		}
 	}
 
@@ -360,61 +353,4 @@ public class ExperimentalSectionParser {
 			}
 		}
 	}
-	
-	/**
-	 * Uses the amount(mols) of reactants to give the correct stoichiometry
-	 */
-	void processReactionStoichiometry() {
-		for (Reaction reaction : reactions) {
-			Map<Chemical, Double> reactantToAmount = new HashMap<Chemical, Double>();
-			for (Chemical reactant : reaction.getReactants()) {
-				if (reactant.getAmountValue()!=null){
-					try{
-						double amount = Double.parseDouble(reactant.getAmountValue());
-						String units = reactant.getAmountUnits();
-						Matcher m = matchAmount.matcher(units);
-						if (m.matches()){
-							String scale = m.group(1);
-							if (scale != null){
-								if (scale.equalsIgnoreCase("m") || scale.equalsIgnoreCase("milli")){
-									amount = amount/1000;
-								}
-								if (scale.equalsIgnoreCase("\u00b5") || scale.equalsIgnoreCase("micro")){
-									amount = amount/1000000;
-								}
-								if (scale.equalsIgnoreCase("n") || scale.equalsIgnoreCase("nano")){
-									amount = amount/1000000000;
-								}
-								if (scale.equalsIgnoreCase("pico")){
-									amount = amount/1000000000000d;	
-								}
-							}
-							reactantToAmount.put(reactant, amount);
-						}
-						
-					}
-					catch (NumberFormatException e) {
-						LOG.trace(reactant.getAmountValue() +" was not a valid double");
-					}
-				}
-			}
-			if (reactantToAmount.keySet().size()>1){
-				double lowestAmount = Double.MAX_VALUE;
-				for (double amountInMols: reactantToAmount.values()) {
-					if (amountInMols < lowestAmount){
-						lowestAmount = amountInMols;
-					}
-				}
-				for (Entry<Chemical, Double> entries: reactantToAmount.entrySet()) {
-					double stoichiometry = 1;
-					if (entries.getValue() != lowestAmount){
-						stoichiometry = entries.getValue()/lowestAmount;
-					}
-					entries.getKey().setStoichiometry(stoichiometry);
-				}
-			}
-		}
-		
-	}
-
 }
