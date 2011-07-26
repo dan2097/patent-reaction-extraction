2a0410af7809d4f6c389cf375e080a809d629a9f
Daniel Lowe <dl387@cam.ac.uk>
2011-07-26 16:38:39 +0100

    Updated method to initialising ChemistryPOSTagger to not use deprecated method

--- src/main/java/dan2097/org/bitbucket/utility/OscarReliantFunctionality.java
+++ src/main/java/dan2097/org/bitbucket/utility/OscarReliantFunctionality.java
@@ -1,9 +1,13 @@
 package dan2097.org.bitbucket.utility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uk.ac.cam.ch.wwmm.chemicaltagger.ChemistryPOSTagger;
 import uk.ac.cam.ch.wwmm.chemicaltagger.OpenNLPTagger;
 import uk.ac.cam.ch.wwmm.chemicaltagger.OscarTokeniser;
 import uk.ac.cam.ch.wwmm.chemicaltagger.RegexTagger;
+import uk.ac.cam.ch.wwmm.chemicaltagger.Tagger;
 import uk.ac.cam.ch.wwmm.oscar.Oscar;
 import uk.ac.cam.ch.wwmm.oscar.chemnamedict.core.ChemNameDictRegistry;
 import uk.ac.cam.ch.wwmm.oscar.opsin.OpsinDictionary;
@@ -24,8 +28,11 @@ public class OscarReliantFunctionality {
 			recogniser.setCprPseudoConfidence(0);
 			recogniser.setOntPseudoConfidence(0);
 			oscar.setRecogniser(recogniser);
-			OscarAndOpsinTagger oscarAndOpsinTagger = new OscarAndOpsinTagger(oscar);
-			posTagger = new ChemistryPOSTagger(new OscarTokeniser(oscar), oscarAndOpsinTagger, new RegexTagger(), OpenNLPTagger.getInstance());
+			List<Tagger> taggersOrderedInDescendingPriority = new ArrayList<Tagger>();
+			taggersOrderedInDescendingPriority.add(new RegexTagger());
+			taggersOrderedInDescendingPriority.add(new OscarAndOpsinTagger(oscar));
+			taggersOrderedInDescendingPriority.add(OpenNLPTagger.getInstance());
+			posTagger = new ChemistryPOSTagger(new OscarTokeniser(oscar), taggersOrderedInDescendingPriority);
 		}
 		 
 		private static class SingletonHolder {
