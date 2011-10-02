40769ec8c3288d6affb8930e7d844d3c93d29b84
Daniel Lowe <dl387@cam.ac.uk>
2011-10-02 14:35:33 +0100

    Attempted to improve recall of solvent identification

--- src/main/java/dan2097/org/bitbucket/reactionextraction/ChemicalRoleAssigner.java
+++ src/main/java/dan2097/org/bitbucket/reactionextraction/ChemicalRoleAssigner.java
@@ -1,9 +1,13 @@
 package dan2097.org.bitbucket.reactionextraction;
 
+import nu.xom.Element;
+import nu.xom.Node;
+
 import org.apache.log4j.Logger;
 
 import dan2097.org.bitbucket.utility.ChemicalTaggerAtrs;
-import nu.xom.Element;
+import dan2097.org.bitbucket.utility.ChemicalTaggerTags;
+import dan2097.org.bitbucket.utility.Utils;
 
 public class ChemicalRoleAssigner {
 
@@ -20,6 +24,9 @@ public class ChemicalRoleAssigner {
 		else if (ChemicalTaggerAtrs.SOLVENT_ROLE_VAL.equals(chemicalEl.getAttributeValue(ChemicalTaggerAtrs.ROLE_ATR))){
 			chemical.setRole(ChemicalRole.solvent);
 		}
+		else if (assignAsSolventDueToInKeyword(chemicalEl, chemical) ){
+			chemical.setRole(ChemicalRole.solvent);
+		}
 		else if (chemicalKnowledge.isKnownCatalystInChI(chemical.getInchi())){
 			chemical.setRole(ChemicalRole.catalyst);
 		}
@@ -29,6 +36,63 @@ public class ChemicalRoleAssigner {
 		else {
 			chemical.setRole(ChemicalRole.reactant);
 		}
+	}
+
+	private static boolean assignAsSolventDueToInKeyword(Element chemicalEl, Chemical chemical) {
+		if (chemicalEl.getLocalName().equals(ChemicalTaggerTags.UNNAMEDMOLECULE_Container) && chemical.getVolumeValue()==null){
+			return false;
+		}
+		if (precededByIn(chemicalEl)){
+			return true;
+		}
+		if (precededByInChemicalAnd(chemicalEl)){
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean precededByInChemicalAnd(Element chemicalEl) {
+		Element previous = Utils.getPreviousElement(chemicalEl);
+		if (previous == null || !previous.getLocalName().equals(ChemicalTaggerTags.CC) || !previous.getValue().equalsIgnoreCase("and")){
+			return false;
+		}
+		Element twoBefore = Utils.getPreviousElement(previous);
+		if (twoBefore!=null){
+			Element molecule = getParent(twoBefore, ChemicalTaggerTags.MOLECULE_Container);
+			if (molecule ==null){
+				 molecule = getParent(twoBefore, ChemicalTaggerTags.UNNAMEDMOLECULE_Container);
+			}
+			if (molecule !=null && precededByIn(molecule)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * The element and the parent element to look for
+	 * @param element
+	 * @param elementName
+	 * @return
+	 */
+	private static Element getParent(Element element, String elementName) {
+		Node parent = element.getParent();
+		while (parent !=null && parent instanceof Element){
+			Element parentEl = (Element)parent;
+			if (parentEl.getLocalName().equals(elementName)){
+				return parentEl;
+			}
+			 parent = parent.getParent();
+		}
+		return null;
+	}
+
+	private static boolean precededByIn(Element chemicalEl) {
+		Element previous = Utils.getPreviousElement(chemicalEl);
+		if (previous != null && previous.getLocalName().equals(ChemicalTaggerTags.IN_IN)){
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/test/java/dan2097/org/bitbucket/reactionextraction/ChemicalRoleAssignmentTest.java
+++ src/test/java/dan2097/org/bitbucket/reactionextraction/ChemicalRoleAssignmentTest.java
@@ -37,4 +37,47 @@ public class ChemicalRoleAssignmentTest {
 		ChemicalRoleAssigner.assignRoleToChemical(chemicalEl, chemical);
 		assertEquals(ChemicalRole.reactant, chemical.getRole());
 	}
+	
+	@Test
+	public void assignedAsSolventInSolventTest(){
+		Element parent = new Element(PREPPHRASE_Container);
+		Element in = new Element(IN_IN);
+		parent.appendChild(in);
+		Element chemicalEl = new Element(MOLECULE_Container);
+		parent.appendChild(chemicalEl);
+		Chemical chemical = new Chemical("foo");
+		ChemicalRoleAssigner.assignRoleToChemical(chemicalEl, chemical);
+		assertEquals(ChemicalRole.solvent, chemical.getRole());
+	}
+	
+	@Test
+	public void assignedAsSolventInChemAndSolventTest(){
+		Element sentence = new Element(SENTENCE_Container);
+		
+		Element prepphrase = new Element(PREPPHRASE_Container);
+		Element in = new Element(IN_IN);
+		prepphrase.appendChild(in);
+		Element chemicalEl1 = new Element(MOLECULE_Container);
+		Element oscarCM = new Element(OSCARCM_Container);
+		Element oscarDashCM = new Element(OSCAR_CM);
+		oscarDashCM.appendChild("foo");
+		oscarCM.appendChild(oscarDashCM);
+		chemicalEl1.appendChild(oscarCM);
+		prepphrase.appendChild(chemicalEl1);
+		
+		Element and = new Element(CC);
+		and.appendChild("and");
+		
+		Element nounphrase = new Element(NOUN_PHRASE_Container);
+		Element chemicalEl2 = new Element(MOLECULE_Container);
+		nounphrase.appendChild(chemicalEl2);
+		Chemical chemical2 = new Chemical("bar");
+		
+		sentence.appendChild(prepphrase);
+		sentence.appendChild(and);
+		sentence.appendChild(nounphrase);
+		
+		ChemicalRoleAssigner.assignRoleToChemical(chemicalEl2, chemical2);
+		assertEquals(ChemicalRole.solvent, chemical2.getRole());
+	}
 }
