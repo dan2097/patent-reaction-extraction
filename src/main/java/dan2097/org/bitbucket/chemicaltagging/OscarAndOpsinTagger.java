6183bd45e9fa7e5916b137c2999a9f11e21d397d
Daniel Lowe <dl387@cam.ac.uk>
2011-10-02 14:48:02 +0100

    Updated to latest version of the OPSIN document extractor

--- src/main/java/dan2097/org/bitbucket/chemicaltagging/OscarAndOpsinTagger.java
+++ src/main/java/dan2097/org/bitbucket/chemicaltagging/OscarAndOpsinTagger.java
@@ -59,9 +59,7 @@ public class OscarAndOpsinTagger extends OscarTagger {
 				}
 			}
 		}
-
-		String[] words = tokenList.toArray(new String[tokenList.size()]);
-		List<IdentifiedChemicalName> identifiedNames = new DocumentToStructures(words).extractNames();
+		List<IdentifiedChemicalName> identifiedNames = new DocumentToStructures(tokenList).extractNames();
 		for (IdentifiedChemicalName ne : identifiedNames) {
 			for (int i = ne.getWordPositionStartIndice(); i <= ne.getWordPositionEndIndice(); i++) {
 				oscarAndOpsinList.set(i, "OSCAR-CM");
